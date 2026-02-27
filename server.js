bbfe7685b7690925135a39de64d5bacf897cab7d
Bryan <bryanr21kelly@gmail.com>
2026-02-27 02:23:52 -0400

    fix port for render

--- server.js
+++ server.js
@@ -25,4 +25,8 @@ app.get("/signal", async (req, res) => {
 
 });
 
-app.listen(3000, () => console.log("API running"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
